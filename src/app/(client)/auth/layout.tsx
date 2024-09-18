cc01518dfcdbca3fcbeeee196f28248aafec2824
kang-theo <theo.cy.kang@gmail.com>
2024-09-18 10:21:50 +0800

    perf: 1. auth layout changes; 2. prisma log transaction options

--- src/app/(client)/auth/layout.tsx
+++ src/app/(client)/auth/layout.tsx
@@ -36,32 +36,6 @@ export default function AuthLayout({
       </div> */}
       {/* hidden */}
       <div className='container relative min-h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0'>
-        <div className='relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r'>
-          <div className='absolute inset-0 bg-[#737373]' />
-          <div className='relative z-20 flex items-center text-lg font-medium'>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              className='mr-2 h-6 w-6'
-            >
-              <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-            </svg>
-            {info.company}
-          </div>
-          <div className='relative z-20 mt-auto'>
-            <blockquote className='space-y-2'>
-              <p className='text-lg'>
-                &ldquo;Leverage our powerful and intuitive tools&rdquo;
-              </p>
-              <footer className='text-sm'>Filmlog&reg;</footer>
-            </blockquote>
-          </div>
-        </div>
         <div className='lg:p-8'>
           <div className='mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]'>
             {children}
@@ -82,6 +56,33 @@ export default function AuthLayout({
               </Link>
               .
             </p>
+          </div>
+        </div>
+        <div className='relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r'>
+          <div className='absolute inset-0 bg-[#737373]' />
+          <div className='relative z-20 flex items-center text-lg font-medium'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              className='mr-2 h-6 w-6'
+            >
+              <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
+            </svg>
+            {info.company}
+          </div>
+          <div className='relative z-20 mt-auto'>
+            <blockquote className='space-y-2'>
+              <p className='text-lg'>
+                &ldquo;Analyze your resume and cover letter to get optimized
+                feedback&rdquo;
+              </p>
+              <footer className='text-sm'>ARM&reg;</footer>
+            </blockquote>
           </div>
         </div>
       </div>
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient;
+  var prisma: PrismaClient | undefined;
 }
 
-const client = globalThis.prisma || new PrismaClient();
+const client =
+  globalThis.prisma ||
+  new PrismaClient({
+    transactionOptions: {
+      isolationLevel: Prisma.TransactionIsolationLevel?.Serializable,
+      maxWait: 5000, // default: 2000
+      timeout: 10000, // default: 5000
+    },
+    log:
+      process.env.NODE_ENV === "production"
+        ? ["error"]
+        : ["query", "info", "warn", "error"],
+  });
 if (process.env.NODE_ENV === "production") globalThis.prisma = client;
 
 export default client;
+
+export { Prisma };
