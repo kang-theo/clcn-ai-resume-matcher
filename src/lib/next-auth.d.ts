e05084775caba0f20e328fb9be75d3398170a809
kang-theo <theo.cy.kang@gmail.com>
2024-10-26 22:33:53 +0800

    perf: extract next-auth definition extension

---
+++ src/lib/next-auth.d.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import NextAuth from "next-auth";
+import { DefaultSession, User as DefaultUser } from "next-auth";
+
+declare module "next-auth" {
+  interface User extends DefaultUser {
+    username: string;
+  }
+  interface Session {
+    user: User & DefaultSession["user"];
+  }
+  interface JWT {
+    id: string;
+    name: string;
+    email: string;
+    username: string;
+    image?: string;
+  }
+}
