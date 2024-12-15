f2b4f0c620bcb6c0b75b0e1c109ff67b312ec93d
kang-theo <theo.cy.kang@gmail.com>
2024-12-15 23:18:58 +0800

    feat: display jobs

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,22 +1,5 @@
 import { AppSidebar } from "@/components/layout/AppSidebar";
-import { auth } from "@/lib/auth";
-import { Session } from "next-auth";
 
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const session: Session | null = await auth();
-  if (!session) {
-    return (
-      <div className='flex h-screen w-screen items-center justify-center'>
-        <p className='text-2xl font-bold'>
-          Please sign in to access this page.
-        </p>
-      </div>
-    );
-  }
-
-  return <AppSidebar session={session}>{children}</AppSidebar>;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <AppSidebar>{children}</AppSidebar>;
 }
