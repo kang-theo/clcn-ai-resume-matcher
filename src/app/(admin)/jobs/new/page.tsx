bab63e76a581c929e5ec49c2408a58ecc46d8c7c
kang-theo <theo.cy.kang@gmail.com>
2024-12-17 13:21:48 +0800

    feat: init new job

---
+++ src/app/(admin)/jobs/new/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { JobForm } from "@/components/jobs/JobForm";
+
+function NewJobPage() {
+  return (
+    <div className='p-4'>
+      <JobForm />
+    </div>
+  );
+}
+
+export default NewJobPage;
